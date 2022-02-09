e03b831c05445a516afbf3dfc3d24c4ef480b830
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-09 19:49:54 +0700

    resolve customer

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -19,9 +19,7 @@ class Auth extends BD_Controller
         $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
-        $this->load->model('M_user', 'user');
-        $this->load->model('M_member', 'member');
-        $this->load->model('M_cashier', 'cashier');
+        $this->load->model('M_customer', 'user');
     }
 
     /**
