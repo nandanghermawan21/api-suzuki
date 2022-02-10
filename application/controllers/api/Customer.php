8fa1c497b90a96969b786276c6aa8cf07ea4b5ed
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-10 07:02:34 +0700

    WITHOUT JWT

--- application/controllers/api/Customer.php
+++ application/controllers/api/Customer.php
@@ -119,7 +119,7 @@ class Customer extends BD_Controller
                     "token" => $headers,
                     "token" => $token,
                     "kunci" => $kunci,
-                    "jwt" => JWT::decode($token, $kunci, array('HS256'))
+                    // "jwt" => JWT::decode($token, $kunci, array('HS256'))
                 ),200);
             } catch (\Exception $e) {
                 $error = new Error_model();
