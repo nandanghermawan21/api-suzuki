ffd7eb24cc5b4e898269bd95e08e2216e0ec3401
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-10 10:33:39 +0700

    commit

--- application/controllers/api/Customer.php
+++ application/controllers/api/Customer.php
@@ -99,9 +99,9 @@ class Customer extends BD_Controller
                     } else {
                         $file = $this->file->upload($path, random_string('alnum', 100) , $media);
                         $customer = $this->customer->fromId($id);
-                        $customer = $customer->imageId = $file->id;
+                        $customer->imageId = $file->id;
                         $customer = $customer->update();
-                        $this->response($file, 200);
+                        $this->response($customer, 200);
                     }
                 }
             } catch (\Exception $e) {
