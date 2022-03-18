50109ea2caf3023ab03df2425a3400739b0839cd
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-03-18 15:22:53 +0700

    test lagi

--- application/controllers/api/Customer.php
+++ application/controllers/api/Customer.php
@@ -51,7 +51,7 @@ class Customer extends BD_Controller
                 $this->response("Phone Is Exist", 400);
             } else {
                 echo("avatar base");
-                echo($customerRegister->avatar);
+                echo($customerRegister);
 
                 //upload image first
                 $file = $this->file->save("useravatar", $customer->username, $customerRegister->avatar);
