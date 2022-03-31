d980d2d790ffc15c472dcb56134f58dae68fc236
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-03-31 23:57:09 +0700

    add update location

---
+++ db/update.sql
@@ -0,0 +1,11 @@
+SELECT * from m_customer
+where username = 'nandanghermawan55'
+
+DELETE from m_customer
+where username = 'dudung123'
+
+ALTER TABLE m_customer
+add lat double
+
+ALTER TABLE m_customer
+add lon double
