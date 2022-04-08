7aa96573e1f752265112f1a0856d3aed0ed7c3d3
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-04-08 20:52:42 +0700

    change query

--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -181,8 +181,8 @@ class Location_model extends CI_Model
 
 	function filterRef(String $filter = "%%"): array
 	{
-		$sql = "(SELECT b.id FROM location_history b WHERE b.ref = a.ref ORDER BY create_date DESC LIMIT 1 ) as id,
-		      a.ref,
+		$sql = "a.ref,
+			  (SELECT b.id FROM location_history b WHERE b.ref = a.ref ORDER BY create_date DESC LIMIT 1 ) as id,
 		      (SELECT b.lat FROM location_history b WHERE b.ref = a.ref ORDER BY create_date DESC LIMIT 1 ) as lat,
 		      (SELECT b.lat FROM location_history b WHERE b.ref = a.ref ORDER BY create_date DESC LIMIT 1 ) as lon,
 		      (SELECT b.lat FROM location_history b WHERE b.ref = a.ref ORDER BY create_date DESC LIMIT 1 ) as direction
