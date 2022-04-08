16c9a5a749db5a0ca9067726174894bbaab28e30
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-04-08 23:05:31 +0700

    commit agein

--- application/models/Location_model.php
+++ application/models/Location_model.php
@@ -199,7 +199,7 @@ class Location_model extends CI_Model
 
 		for ($i = 0; $i < count($result); $i++) {
 			$location = $this->fromRow($result[$i]);
-			$$data[0] = $location;
+			$$data[0] = $location->toJson();
 		}
 
 		return $data;
