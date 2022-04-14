31507b6f02d63a152a2884bd75d2292f3610c5e6
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-04-14 16:35:50 +0700

    add chat

---
+++ application/controllers/api/Chat.php
@@ -0,0 +1,57 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Chat extends BD_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Chat_model', 'chat');
+    }
+
+
+    /**
+     * @OA\post(path="/api/Service/savelocation",tags={"chat"},
+     *   operationId="Send Message TO Customer",
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/ComposeChat")
+     *     ),
+     *   ),
+     *   @OA\Response(response=200,
+     *     description="save location",
+     *     @OA\JsonContent(
+     *       @OA\Items(ref="#/components/schemas/ChatModel")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function savelocation_post()
+    {
+        $jsonBody  = json_decode(file_get_contents('php://input'), true);
+        $chat = $this->chat->fromJson($jsonBody);
+
+        if ($this->getData() == null) {
+            $this->response("unautorized", 401);
+        } else {
+            try {
+                $chat->messageType = $this->getData()->type . "-To-" . "customer";
+                $chat->sender = "customer-" . $this->getData()->id;
+                $chat->receiver = "customer-" . $chat->receiver;
+
+                //save message
+                $chat = $chat->add();
+                $this->response($chat->toJson(), 200);
+            } catch (\Exception $e) {
+                $error = new Error_model();
+                $error->status = 500;
+                $error->message = $e->getMessage();
+                $this->response($error->message, 500);
+            }
+        }
+    }
+}
---
+++ application/models/Chat_model.php
@@ -0,0 +1,198 @@
+<?php if (!defined('BASEPATH')) exit('No direct script allowed');
+
+/**
+ * @OA\Schema(schema="ChatModel")
+ */
+class Chat_model extends CI_Model
+{
+    private $tableName = "chat";
+
+    /**
+     * @OA\Property()
+     * @var int
+     */
+    public $id;
+    private function idField(): string
+    {
+        return "id";
+    }
+    public function idJsonKey(): string
+    {
+        return "id";
+    }
+
+    /**
+     * @OA\Property()
+     * @var DateTime
+     */
+    public $createDate;
+    private function createDateField(): string
+    {
+        return "create_date";
+    }
+    public function createDateJsonKey(): string
+    {
+        return "createDate";
+    }
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $messageType;
+    private function messageTypeField(): string
+    {
+        return "message_type";
+    }
+    public function messageTypeJsonKey(): string
+    {
+        return "messageType";
+    }
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $sender;
+    private function senderField(): string
+    {
+        return "message_type";
+    }
+    public function senderJsonKey(): string
+    {
+        return "sender";
+    }
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $receiver;
+    private function receiverField(): string
+    {
+        return "message_type";
+    }
+    public function receiverJsonKey(): string
+    {
+        return "receiver";
+    }
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $message;
+    private function messageField(): string
+    {
+        return "message_type";
+    }
+    public function messageJsonKey(): string
+    {
+        return "message";
+    }
+
+    public function  fromJson($json): Chat_model
+	{
+		$data = new Chat_model();
+
+		if (isset($json[$this->idJsonKey()])) {
+			$data->id = $json[$this->idJsonKey()];
+		}
+		if (isset($json[$this->messageTypeJsonKey()])) {
+			$data->messageType = $json[$this->messageTypeJsonKey()];
+		}
+		if (isset($json[$this->createDateJsonKey()])) { //DateTime::ATOM
+			$data->createDate =  date_create(date(DATE_ATOM, strtotime($json[$this->createDateJsonKey()])));  //  DateTime::createFromFormat(DateTime::ISO8601, $json[$this->createDateJsonKey()]);
+		}
+        if (isset($json[$this->senderJsonKey()])) {
+			$data->sender = $json[$this->senderJsonKey()];
+		}
+        if (isset($json[$this->receiverJsonKey()])) {
+			$data->receiver = $json[$this->receiverJsonKey()];
+		}
+        if (isset($json[$this->messageJsonKey()])) {
+			$data->message = $json[$this->messageJsonKey()];
+		}
+		
+		return $data;
+	}
+
+    public function fromRow($row): Chat_model
+	{
+		$data = new Chat_model();
+		$data->id = $row->{$this->idField()};
+		$data->messageType = $row->{$this->messageTypeField()};
+		$data->createDate = date_create(date(DATE_ATOM, strtotime($row->{$this->createDateField()})));
+		$data->sender = $row->{$this->senderField()};
+		$data->receiver = $row->{$this->receiverField()};
+		$data->message = $row->{$this->messageField()};
+
+		return $data;
+	}
+
+    public function toArray(): array
+	{
+
+		$data = array(
+			$this->idField() => $this->id,
+			$this->messageField() => $this->messageType,
+			$this->createDateField() => $this->createDate->format(DATE_ATOM),  // date_format($this->createDate, DateTime::ATOM),
+			$this->senderField() => $this->sender,
+			$this->receiverField() => $this->receiver,
+			$this->messageField() => $this->message,
+		);
+
+		return $data;
+	}
+
+    public function toJson(): array
+	{
+		$data = array(
+			$this->idJsonKey() => (int) $this->id,
+			$this->messageTypeJsonKey() => $this->messageType,
+			$this->createDateJsonKey() => $this->createDate->format(DateTime::ATOM),
+			$this->senderJsonKey() => (float) $this->sender,
+			$this->receiverJsonKey() =>  (float) $this->receiver,
+			$this->messageJsonKey() =>  (float) $this->message,
+		);
+
+		return $data;
+	}
+
+    public function  add(): Chat_model
+	{
+		try {
+			//generate key
+			$this->id = null;
+
+			$this->db->insert($this->tableName, $this->toArray());
+
+			$data = $this->db->get_where($this->tableName, array(
+				$this->idField() => $this->db->insert_id()
+
+			));
+			$result = $data->result();
+			return $this->fromRow($result[0]);
+		} catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+
+/**
+ * @OA\Schema(schema="ComposeChat")
+ */
+class CustomerRegister
+{
+	/**
+	 * @OA\Property()
+	 * @var int
+	 */
+	public $receiver;
+
+	/**
+	 * @OA\Property()
+	 * @var String
+	 */
+	public $message;
+}
