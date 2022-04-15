b90ab2123bbebe1458629ce67775e0e0708dfe27
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-04-15 14:08:02 +0700

    commit

--- application/models/Notification_model.php
+++ application/models/Notification_model.php
@@ -10,15 +10,15 @@ class Notification_model extends CI_Model
         curl_setopt($curl_handle, CURLOPT_URL, 'https://onesignal.com/api/v1/notifications');
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl_handle, CURLOPT_POST, 1);
-        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, array(
-            "app_id" => $appId,
-            "include_player_ids" => $receiver,
-            // "contents" => $message,
-            // "headings" => $title,
-            "app_url" => $appUrl,
-            "ios_sound" => "my_notification_sound.wav",
-            "android_channel_id" => "6d52b765-dc9f-4735-a38b-77b784dbe90b"
-        ));
+        // curl_setopt($curl_handle, CURLOPT_POSTFIELDS, array(
+        //     "app_id" => $appId,
+        //     "include_player_ids" => $receiver,
+        //     // "contents" => $message,
+        //     // "headings" => $title,
+        //     "app_url" => $appUrl,
+        //     "ios_sound" => "my_notification_sound.wav",
+        //     "android_channel_id" => "6d52b765-dc9f-4735-a38b-77b784dbe90b"
+        // ));
 
         curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
             "Accept: application/json",
